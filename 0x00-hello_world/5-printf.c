f66bc0c984a050c5b455a8a9e584fdbcf8b00554
michaelmutunga <michaelmutunga4105@gmail.com>
2023-10-05 00:23:49 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a piece of art with proper grammar.
+ *
+ * Return: Always 0 (indicating success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
