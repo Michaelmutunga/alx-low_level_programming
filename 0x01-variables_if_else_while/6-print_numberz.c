0e1affd4dd55e8a027f4a7340101a94911e59cf0
michaelmutunga <michaelmutunga4105@gmail.com>
2023-10-06 14:17:06 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints single-digit numbers in base 10
+ *              starting from 0, followed by a new line, using only putchar.
+ *
+ * Return: Always 0 (indicating success)
+ */
+int main(void)
+{
+int number = 0;
+
+while (number < 10)
+{
+putchar(number + 48); 
+number++;
+}
+
+putchar('\n');
+
+return (0);
+}
